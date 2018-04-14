55e5e1482b4f1a6f09e446916c2f573162b4f76d
user1 <rose18.2013@gmail.com>
2018-04-14 12:34:16 +0300

    added

--- task1.py
+++ task1.py
@@ -1,1 +1,12 @@
+import math
+print ('R')
+R=input()
+R=float(R)
+print ('L')
+L=input()
+L=float(L)
+V=R**2*L*math.pi
+print ('volume', round (V,3))
+
+
 
--- task2.py
+++ task2.py
@@ -1,1 +1,10 @@
-
+import scipy.optimize
+import math
+import time
+def f(x):
+    return math.cos(x)/(1+x**2)
+def derivative(x):
+    return (-math.sin(x)/(1+x**2))-((math.cos(x)*2*X)/(1+x**2)**2)
+print (scipy.optimize.bisect(f(x),0.1,2.4))
+start_time=time.time()
+for i in range(10000):
